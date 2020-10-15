4817dd5aaefcb1ca424d8ec1ea1f89c30851814e
Peter Kim <petekim@gmail.com>
2020-10-15 08:57:14 -0700

    Add fillna and str UPPER-CASE

--- pandas-col-dtype.py
+++ pandas-col-dtype.py
@@ -19,3 +19,16 @@ col_str = df.select_dtypes(include=object).columns
 
 print('Numeric columns: ', col_numeric)
 print('String columns: ', col_str)
+
+# fill nulls by column type
+df_clean = df.copy()
+_ = [df_clean[each_col].fillna(0, inplace=True) for each_col in col_numeric]
+_ = [df_clean[each_col].fillna("", inplace=True) for each_col in col_str]
+
+print("Fill nulls operation completed.", "\n")
+
+# convert str to UPPER-CASE
+for each_col in col_str:
+    df_clean[each_col] = df_clean[each_col].str.upper()
+    
+print("Modify str to UPPER-CASE complete.", "\n")
